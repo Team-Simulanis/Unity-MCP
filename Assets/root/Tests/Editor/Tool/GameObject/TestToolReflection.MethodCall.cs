f8106232c3d0d677a8f8c0a24b0fc0cfd89626f0
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-05-10 02:12:25 -0700

    refactor: Rename test methods for clarity and improve result validation

--- Assets/root/Tests/Editor/Tool/GameObject/TestToolReflection.MethodCall.cs
+++ Assets/root/Tests/Editor/Tool/GameObject/TestToolReflection.MethodCall.cs
@@ -8,29 +8,51 @@ namespace com.IvanMurzak.Unity.MCP.Editor.Tests
     public partial class TestToolReflection
     {
         [UnityTest]
-        public IEnumerator MethodCall_UnityEditor_EditorUserBuildSettings_activeBuildTarget()
+        public IEnumerator MethodCall_UnityEditor_EditorUserBuildSettings_get_activeBuildTarget()
         {
             var classType = typeof(UnityEditor.EditorUserBuildSettings);
             var name = "get_" + nameof(UnityEditor.EditorUserBuildSettings.activeBuildTarget);
             var methodInfo = classType.GetMethod(name);
 
-            var result = new Tool_Reflection().MethodCall(
-                filter: new MethodPointerRef(methodInfo));
+            ResultValidation(new Tool_Reflection().MethodCall(
+                filter: new MethodPointerRef(methodInfo)));
 
-            ResultValidation(result);
             yield return null;
         }
         [UnityTest]
-        public IEnumerator MethodCall_UnityEditor_Build_NamedBuildTarget_TargetName()
+        public IEnumerator MethodCall_UnityEditor_Build_NamedBuildTarget_get_TargetName()
         {
             var classType = typeof(UnityEditor.Build.NamedBuildTarget);
             var name = "get_" + nameof(UnityEditor.Build.NamedBuildTarget.TargetName);
             var methodInfo = classType.GetMethod(name);
 
-            var result = new Tool_Reflection().MethodCall(
-                filter: new MethodPointerRef(methodInfo));
+            ResultValidation(new Tool_Reflection().MethodCall(
+                filter: new MethodPointerRef(methodInfo)));
 
-            ResultValidation(result);
+            yield return null;
+        }
+        [UnityTest]
+        public IEnumerator MethodCall_UnityEngine_Application_get_platform()
+        {
+            var classType = typeof(UnityEngine.Application);
+            var name = "get_" + nameof(UnityEngine.Application.platform);
+            var methodInfo = classType.GetMethod(name);
+            var methodPointerRef = new MethodPointerRef(methodInfo);
+
+            UnityEngine.Debug.Log($"Input: {methodPointerRef}\n");
+
+            ResultValidation(new Tool_Reflection().MethodCall(
+                filter: methodPointerRef));
+
+            ResultValidation(new Tool_Reflection().MethodCall(
+                filter: methodPointerRef,
+                executeInMainThread: true));
+
+            ResultValidation(new Tool_Reflection().MethodCall(
+                filter: methodPointerRef,
+                executeInMainThread: true,
+                methodNameMatchLevel: 6));
+
             yield return null;
         }
     }
