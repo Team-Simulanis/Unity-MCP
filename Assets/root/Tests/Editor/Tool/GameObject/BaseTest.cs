386ba4479d65479c9fa8ff3b4af47e735b4ac9d3
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-05-11 15:17:12 -0700

    feat: Add method to destroy all game objects in the active scene during teardown

--- Assets/root/Tests/Editor/Tool/GameObject/BaseTest.cs
+++ Assets/root/Tests/Editor/Tool/GameObject/BaseTest.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.TestTools;
 
 namespace com.IvanMurzak.Unity.MCP.Editor.Tests
@@ -19,7 +20,17 @@ namespace com.IvanMurzak.Unity.MCP.Editor.Tests
         public virtual IEnumerator TearDown()
         {
             Debug.Log($"[{GetType().Name}] TearDown");
+
+            DestroyAllGameObjectsInActiveScene();
+
             yield return null;
+        }
+
+        protected static void DestroyAllGameObjectsInActiveScene()
+        {
+            var scene = SceneManager.GetActiveScene();
+            foreach (var go in scene.GetRootGameObjects())
+                Object.DestroyImmediate(go);
         }
     }
 }
