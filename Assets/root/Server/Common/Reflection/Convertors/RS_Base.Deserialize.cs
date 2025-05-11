34eeae4aecc26768adb8b5b30b33dd5fa5d30aba
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2025-05-11 01:11:30 -0700

    feat: Enhance Deserialize method to handle fields and properties assignment

--- Assets/root/Server/Common/Reflection/Convertors/RS_Base.Deserialize.cs
+++ Assets/root/Server/Common/Reflection/Convertors/RS_Base.Deserialize.cs
@@ -1,4 +1,5 @@
 #pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+using System.Reflection;
 using com.IvanMurzak.Unity.MCP.Common.Data.Utils;
 using com.IvanMurzak.Unity.MCP.Common.Utils;
 
@@ -14,7 +15,48 @@ namespace com.IvanMurzak.Unity.MCP.Common.Reflection.Convertor
 
             var result = data.valueJsonElement != null
                 ? JsonUtils.Deserialize(data.valueJsonElement.Value, type)
-                : null;
+                : TypeUtils.GetDefaultValue(type);
+
+            if (data.fields != null)
+            {
+                foreach (var field in data.fields)
+                {
+                    if (string.IsNullOrEmpty(field.name))
+                        continue;
+
+                    var fieldType = TypeUtils.GetType(field.type);
+                    if (fieldType == null)
+                        continue;
+
+                    var fieldValue = field.valueJsonElement != null
+                        ? JsonUtils.Deserialize(field.valueJsonElement.Value, fieldType)
+                        : TypeUtils.GetDefaultValue(fieldType);
+
+                    var fieldInfo = type.GetField(field.name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                    if (fieldInfo != null)
+                        fieldInfo.SetValue(result, fieldValue);
+                }
+            }
+            if (data.properties != null)
+            {
+                foreach (var property in data.properties)
+                {
+                    if (string.IsNullOrEmpty(property.name))
+                        continue;
+
+                    var fieldType = TypeUtils.GetType(property.type);
+                    if (fieldType == null)
+                        continue;
+
+                    var fieldValue = property.valueJsonElement != null
+                        ? JsonUtils.Deserialize(property.valueJsonElement.Value, fieldType)
+                        : TypeUtils.GetDefaultValue(fieldType);
+
+                    var propertyInfo = type.GetProperty(property.name, BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance);
+                    if (propertyInfo != null && propertyInfo.CanWrite)
+                        propertyInfo.SetValue(result, fieldValue);
+                }
+            }
 
             return result;
         }
